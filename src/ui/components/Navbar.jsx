dd6b147877d2712ed7bdf6630b8145317a4ba3cf
martinrdrz <martinrdrz@hotmail.com>
2024-05-24 18:46:13 -0300

    agregado de ruteo de  login y Logout

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,28 +1,36 @@
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useNavigate, useNavigation } from 'react-router-dom';
 
 export const Navbar = () => {
+    const navigate = useNavigate();
+
+    const onLogout = () => {
+        navigate('/login', { replace: true });
+    };
+
     return (
-        <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
-            <Link className="navbar-brand" to="/">
+        <nav className='navbar navbar-expand-sm navbar-dark bg-dark p-2'>
+            <Link className='navbar-brand' to='/'>
                 Asociaciones
             </Link>
 
-            <div className="navbar-collapse">
-                <div className="navbar-nav">
-                    <NavLink className={({ isActive }) => `nav-item nav-link ${isActive ? 'active' : ''}`} to="/marvel">
+            <div className='navbar-collapse'>
+                <div className='navbar-nav'>
+                    <NavLink className={({ isActive }) => `nav-item nav-link ${isActive ? 'active' : ''}`} to='/marvel'>
                         Marvel
                     </NavLink>
 
-                    <NavLink className={({ isActive }) => `nav-item nav-link ${isActive ? 'active' : ''}`} to="/dc">
+                    <NavLink className={({ isActive }) => `nav-item nav-link ${isActive ? 'active' : ''}`} to='/dc'>
                         DC
                     </NavLink>
                 </div>
             </div>
 
-            <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
-                <ul className="navbar-nav ml-auto">
-                    <span className="nav-item nav-link text-primary">Martin</span>
-                    <button className="nav-item nav-link btn">logout</button>
+            <div className='navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end'>
+                <ul className='navbar-nav ml-auto'>
+                    <span className='nav-item nav-link text-primary'>Martin</span>
+                    <button className='nav-item nav-link btn' onClick={onLogout}>
+                        logout
+                    </button>
                 </ul>
             </div>
         </nav>
