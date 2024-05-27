a4641c93c72836f1d2a9d005c7c38e18f06b28a8
martinrdrz <martinrdrz@hotmail.com>
2024-05-27 17:14:22 -0300

    listado basico heroes

---
+++ src/heroes/components/HeroList.jsx
@@ -0,0 +1,13 @@
+import { getHeroesByPublisher } from '../helpers';
+
+export const HeroList = ({ publisher }) => {
+    const heroes = getHeroesByPublisher(publisher);
+
+    return (
+        <ul>
+            {heroes.map((heroe) => {
+                <li key={heroe.id}>{heroe.superhero}</li>;
+            })}
+        </ul>
+    );
+};
